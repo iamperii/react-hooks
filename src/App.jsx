15b6ba124de33bb8c5cd79286e6a5459f26c52c7
Memmedova Peri <perimemmedova20052022@gmail>
2025-03-10 22:10:53 +0400

    use-reducer hook done.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import { StateExample } from './hooks/use-state/index';
 import { EffectExample } from './hooks/use-effect/index';
 import { ContextExample } from './hooks/use-context';
+import { ReducerExample } from './hooks/use-reducer';
 
 const App = () => {
 	return (
 		<>
 			{/* <StateExample /> */}
 			{/* <EffectExample /> */}
-			<ContextExample />
+			{/* <ContextExample /> */}
+			<ReducerExample />
 		</>
 	);
 };
--- src/hooks/use-reducer/index.jsx
+++ src/hooks/use-reducer/index.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'increment':
+			return { count: state.count + 1 };
+		case 'decrement':
+			return {
+				count: state.count - 1,
+			};
+		case 'double':
+			return {
+				count: state.count * 2,
+			};
+		default:
+			console.log('No action type');
+	}
+};
 
 export const ReducerExample = () => {
-	const [count, setCount] = useState(0);
+	const [state, dispatch] = useReducer(reducer, { count: 0 });
+
 	return (
 		<>
 			<div>
-				<p style={{ textAlign: 'center' }}>Count: {count} </p>
-				<button onClick={() => setCount((prev) => prev + 1)}>+</button>
-				<button onClick={() => setCount((prev) => prev - 1)}>-</button>
-				<button onClick={() => setCount((prev) => prev * 2)}>*</button>
+				<p style={{ textAlign: 'center' }}>Count: {state.count} </p>
+				<button onClick={() => dispatch({ type: 'increment' })}>+</button>
+				<button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+				<button onClick={() => dispatch({ type: 'double' })}>*</button>
 			</div>
 		</>
 	);
